493e4c54f8806003253a4277992f04a4bfd71b08
채지원 <112457971+wondevc@users.noreply.github.com>
2025-02-27 20:57:35 +0900

    feat: init SpringBoot-3.3.7 WebFlux Application

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,7 @@
 rootProject.name = "spring-bean-creation-deadlock-test"
+
+include(
+    ":application-spring-boot-337",
+)
+
+project(":application-spring-boot-337").projectDir = file("application/sb-337")
